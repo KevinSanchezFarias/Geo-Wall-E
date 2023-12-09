15f90c43e89099d9f73c1c58b7387828150aa8ce
KevinSanchezFarias <132581589+KevinSanchezFarias@users.noreply.github.com>
2023-12-09 11:18:49 -0500

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace WaLI;
+﻿namespace FormsTest1;
 
-partial class MainForm
+partial class Form1
 {
     /// <summary>
     ///  Required designer variable.
@@ -17,7 +17,6 @@ partial class MainForm
         {
             components.Dispose();
         }
-
         base.Dispose(disposing);
     }
 
@@ -29,10 +28,58 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
+        //Background
+        this.BackgroundImage = Image.FromFile(@"C:\Users\DELL\Desktop\PEpe\FormsTest1\X.jpg");
+        this.BackgroundImageLayout = ImageLayout.Stretch;
+                
+        //Botones
+        //Button button1 = new Button();
+        //Button button2 = new Button();
+        button1.Anchor = AnchorStyles.Left | AnchorStyles.Top ;
+        button2.Anchor = AnchorStyles.Left | AnchorStyles.Top ;
+        button2.Image = new Bitmap(@"C:\Users\DELL\Desktop\PEpe\FormsTest1\trash.png");
+        button1.Image = new Bitmap(@"C:\Users\DELL\Desktop\PEpe\FormsTest1\play.png");
+        button1.Image = (Image)(new Bitmap(button1.Image, new Size(20, 20)));
+        button2.Image = (Image)(new Bitmap(button2.Image, new Size(20, 20)));
+        button1.ImageAlign = ContentAlignment.MiddleCenter;
+        button2.ImageAlign = ContentAlignment.MiddleCenter;
+        button2.Location = new Point(100, 120);
+        button1.Location = new Point(20, 120); 
+        
+        //Panel para graficar
+        //Panel panel1 = new Panel();
+        panel1.Anchor = AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+        panel1.BackColor = System.Drawing.Color.FromArgb(1,1,1,0);
+        panel1.Location = new System.Drawing.Point(this.ClientSize.Width / 2, 0);
+        panel1.Size = new System.Drawing.Size(this.ClientSize.Width / 2, this.ClientSize.Height);
+
+        //Text box
+        this.textBox1 = new System.Windows.Forms.TextBox();
+        this.textBox1.Size = new System.Drawing.Size(200, 90); 
+        textBox1.Location = new Point(20, 20);
+        textBox1.Anchor = AnchorStyles.Left | AnchorStyles.Top ;       
+        this.textBox1.Multiline = true;
+        this.textBox1.AcceptsReturn = true;
+        this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+
+        //Form1
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        //this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Text = "Geo Wall-E";
+
+        
+        this.textBox1.Location = new System.Drawing.Point(20,20);
+        this.textBox1.Anchor = AnchorStyles.None;
+        button1.Anchor = AnchorStyles.None;
+        button2.Anchor = AnchorStyles.None;
+
+        this.Controls.Add(panel1);
+        this.Controls.Add(button1);  
+        this.Controls.Add(button2);
+        this.Controls.Add(this.textBox1);
+
+        this.Controls.SetChildIndex(textBox1, 0);
     }
 
     #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,57 +1,67 @@
-namespace WaLI;
+namespace FormsTest1;
 
-public partial class MainForm : Form
+public partial class Form1 : Form
 {
-    readonly TextBox textBox;
-    readonly Button button;
-    readonly Label resultLabel;
+    private TextBox textBox1;
+    private Button button1 = new Button();
+    private Button button2 = new Button();
+    private Panel panel1 = new Panel();
 
-    /// <summary>
-    /// Represents the main form of the application.
-    /// </summary>
-    public MainForm()
+    public Form1()
     {
-        InitializeComponent();
-
-        // Initialize the TextBox and add it to the Form's controls
-        textBox = new TextBox
-        {
-            Location = new Point(10, 10), // Set the location of the TextBox
-            Size = new Size(200, 20) // Set the size of the TextBox
-        };
-        this.Controls.Add(textBox);
-
-        // Initialize the Button, set its text, and add it to the Form's controls
-        button = new Button
-        {
-            Text = "Submit",
-            Location = new Point(10, 40), // Set the location of the Button
-            Size = new Size(100, 20) // Set the size of the Button
-        };
-        this.Controls.Add(button);
-
-        // Initialize the Label and add it to the Form's controls
-        resultLabel = new Label
-        {
-            Location = new Point(10, 70) // Set the location of the Label
-        };
-        this.Controls.Add(resultLabel);
-
-        // Attach an event handler to the Button's Click event
-        button.Click += Button_Click!;
+             
+        InitializeComponent(); 
+        
+        textBox1.Enter += new EventHandler(textBox1_Enter);
+        textBox1.Leave += new EventHandler(textBox1_Leave);
+        textBox1.Text = "Introduzca sus instrucciones";
+        textBox1.ForeColor = System.Drawing.Color.Gray;
+        this.Resize += new EventHandler(Form1_SizeChanged);
+        button1.Click += new EventHandler(SubmitCommands);
+        button2.Click += new EventHandler(ClearGraphics);
     }
-    /// <summary>
-    /// Event handler for the button click event.
-    /// Calls the GSharp function with the text from the TextBox and sets the Text property of the Label to the result.
-    /// </summary>
-    /// <param name="sender">The object that raised the event.</param>
-    /// <param name="e">The event arguments.</param>
-    private void Button_Click(object sender, EventArgs e)
+
+    
+     private void Form1_SizeChanged(object sender, EventArgs e)
+     {
+        AdjustDynamicPanelSize();
+        this.button2.Location = new Point(20,120);
+        this.button1.Location = new Point(120,120); 
+        this.textBox1.Location = new System.Drawing.Point(20,20);
+     }
+
+     private void AdjustDynamicPanelSize()
+        {
+            this.panel1.Size = new System.Drawing.Size(this.ClientSize.Width/2, this.ClientSize.Height);
+            this.panel1.Location = new System.Drawing.Point(this.ClientSize.Width/2, 0);
+        }
+
+    private void textBox1_Enter(object sender, EventArgs e)// quitar el prompt
     {
-        // Call the GSharp function with the text from the TextBox
-        object result = MiddleEnd.GSharp(textBox.Text);
-
-        // Set the Text property of the Label to the result
-        resultLabel.Text = result.ToString();
+    if (textBox1.Text == "Introduzca sus instrucciones")
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = System.Drawing.Color.Black; 
+        }
     }
+
+    private void textBox1_Leave(object sender, EventArgs e)//poner el prompt
+    {
+    if (string.IsNullOrWhiteSpace(textBox1.Text))
+        {
+            textBox1.Text = "Introduzca sus instrucciones";
+            textBox1.ForeColor = System.Drawing.Color.Gray;
+        }
+    }
+
+    private void SubmitCommands(object sender, EventArgs e)
+    {
+        //mandar el texto al interprete
+    }
+
+    private void ClearGraphics(object sender, EventArgs e)
+    {
+        //Limpiar el panel de graficos
+    }
+
 }
