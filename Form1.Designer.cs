13d508d543c57270d45bd675a608857686b1f97e
KevinSanchezFarias <132581589+KevinSanchezFarias@users.noreply.github.com>
2023-12-13 09:42:00 -0500

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,39 +28,46 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+
         //Background
-        this.BackgroundImage = Image.FromFile(@"C:\Users\DELL\Desktop\PEpe\FormsTest1\X.jpg");
+        this.BackgroundImage = Image.FromFile(@"img\X.jpg");
         this.BackgroundImageLayout = ImageLayout.Stretch;
                 
-        //Botones
-        //Button button1 = new Button();
-        //Button button2 = new Button();
-        button1.Anchor = AnchorStyles.Left | AnchorStyles.Top ;
-        button2.Anchor = AnchorStyles.Left | AnchorStyles.Top ;
-        button2.Image = new Bitmap(@"C:\Users\DELL\Desktop\PEpe\FormsTest1\trash.png");
-        button1.Image = new Bitmap(@"C:\Users\DELL\Desktop\PEpe\FormsTest1\play.png");
-        button1.Image = (Image)(new Bitmap(button1.Image, new Size(20, 20)));
-        button2.Image = (Image)(new Bitmap(button2.Image, new Size(20, 20)));
-        button1.ImageAlign = ContentAlignment.MiddleCenter;
-        button2.ImageAlign = ContentAlignment.MiddleCenter;
-        button2.Location = new Point(100, 120);
-        button1.Location = new Point(20, 120); 
+        //Boton 1        
+        this.button1.Image = new Bitmap(@"img\play.png");
+        this.button1.ImageAlign = ContentAlignment.MiddleCenter;
+        this.button1.Image = (Image)(new Bitmap(this.button1.Image, new Size(20, 20)));
+        this.button1.Location = new Point(20, 120);
+        this.button1.Anchor = AnchorStyles.Left | AnchorStyles.Top ;
+        button1.Click += new EventHandler(SubmitCommands);
+        //Boton 2
+        this.button2.Image = new Bitmap(@"img\trash.png");       
+        this.button2.ImageAlign = ContentAlignment.MiddleCenter;
+        this.button2.Image = (Image)(new Bitmap(this.button2.Image, new Size(20, 20)));
+        this.button2.Location = new Point(100, 120);         
+        this.button2.Anchor = AnchorStyles.Left | AnchorStyles.Top ;        
+        button2.Click += new System.EventHandler(ClearGraphics);
         
         //Panel para graficar
-        //Panel panel1 = new Panel();
-        panel1.Anchor = AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
-        panel1.BackColor = System.Drawing.Color.FromArgb(1,1,1,0);
-        panel1.Location = new System.Drawing.Point(this.ClientSize.Width / 2, 0);
-        panel1.Size = new System.Drawing.Size(this.ClientSize.Width / 2, this.ClientSize.Height);
+        //Panel panel = new Panel();
+        panel.Anchor = AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
+        panel.BackColor = System.Drawing.Color.FromArgb(1,1,1,0);
+        panel.Location = new System.Drawing.Point(this.ClientSize.Width / 2, 0);
+        panel.Size = new System.Drawing.Size(this.ClientSize.Width / 2, this.ClientSize.Height);
 
         //Text box
         this.textBox1 = new System.Windows.Forms.TextBox();
-        this.textBox1.Size = new System.Drawing.Size(200, 90); 
-        textBox1.Location = new Point(20, 20);
-        textBox1.Anchor = AnchorStyles.Left | AnchorStyles.Top ;       
+        this.textBox1.Size = new System.Drawing.Size(this.Height/3, this.Width/5); 
+        this.textBox1.Location = new Point(20, 20);
+        this.textBox1.Anchor = AnchorStyles.Left | AnchorStyles.Top ;       
         this.textBox1.Multiline = true;
         this.textBox1.AcceptsReturn = true;
         this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        this.textBox1.Enter += new System.EventHandler(textBox1_Enter);
+        this.textBox1.Leave += new System.EventHandler(textBox1_Leave);
+        this.textBox1.Text = "Introduzca sus instrucciones";
+        this.textBox1.ForeColor = System.Drawing.Color.Gray;
+        
 
         //Form1
         this.components = new System.ComponentModel.Container();
@@ -71,15 +78,16 @@ partial class Form1
         
         this.textBox1.Location = new System.Drawing.Point(20,20);
         this.textBox1.Anchor = AnchorStyles.None;
-        button1.Anchor = AnchorStyles.None;
-        button2.Anchor = AnchorStyles.None;
+        this.button1.Anchor = AnchorStyles.None;
+        this.button2.Anchor = AnchorStyles.None;
 
-        this.Controls.Add(panel1);
-        this.Controls.Add(button1);  
-        this.Controls.Add(button2);
+        this.Controls.Add(panel);
+        this.Controls.Add(this.button1);  
+        this.Controls.Add(this.button2);
         this.Controls.Add(this.textBox1);
 
         this.Controls.SetChildIndex(textBox1, 0);
+        this.Resize += new EventHandler(Form1_SizeChanged);
     }
 
     #endregion
--- Form1.cs
+++ Form1.cs
@@ -2,38 +2,33 @@ namespace FormsTest1;
 
 public partial class Form1 : Form
 {
-    private TextBox textBox1;
+    private TextBox textBox1 ;
     private Button button1 = new Button();
     private Button button2 = new Button();
-    private Panel panel1 = new Panel();
+    private PictureBox panel = new PictureBox();
 
     public Form1()
     {
              
         InitializeComponent(); 
+        this.WindowState = FormWindowState.Maximized;  
         
-        textBox1.Enter += new EventHandler(textBox1_Enter);
-        textBox1.Leave += new EventHandler(textBox1_Leave);
-        textBox1.Text = "Introduzca sus instrucciones";
-        textBox1.ForeColor = System.Drawing.Color.Gray;
-        this.Resize += new EventHandler(Form1_SizeChanged);
-        button1.Click += new EventHandler(SubmitCommands);
-        button2.Click += new EventHandler(ClearGraphics);
     }
 
     
-     private void Form1_SizeChanged(object sender, EventArgs e)
+     private void Form1_SizeChanged(object sender, EventArgs e)//responsivo
      {
-        AdjustDynamicPanelSize();
-        this.button2.Location = new Point(20,120);
-        this.button1.Location = new Point(120,120); 
-        this.textBox1.Location = new System.Drawing.Point(20,20);
+        AdjustDynamicCanvasSize();
+        this.button2.Location = new Point(20,this.Height/3 + 30);
+        this.button1.Location = new Point(this.Width/5 - 60,this.Height/3 + 30); 
+        this.textBox1.Location = new Point(20,20);
+        this.textBox1.Size = new System.Drawing.Size(this.Width/5, this.Height/3);
      }
 
-     private void AdjustDynamicPanelSize()
+     private void AdjustDynamicCanvasSize()
         {
-            this.panel1.Size = new System.Drawing.Size(this.ClientSize.Width/2, this.ClientSize.Height);
-            this.panel1.Location = new System.Drawing.Point(this.ClientSize.Width/2, 0);
+            this.panel.Size = new System.Drawing.Size(this.ClientSize.Width/2, this.ClientSize.Height);
+            this.panel.Location = new System.Drawing.Point(this.ClientSize.Width/2, 0);
         }
 
     private void textBox1_Enter(object sender, EventArgs e)// quitar el prompt
@@ -56,12 +51,97 @@ public partial class Form1 : Form
 
     private void SubmitCommands(object sender, EventArgs e)
     {
-        //mandar el texto al interprete
+        Graphics canvas = panel.CreateGraphics();
+        foreach (ToDraw graphic in GSharp(textBox1.Text))
+        {
+            switch (graphic.figure.ToLower)
+            {
+                case "point":
+                {Draw_Point(graphic.color,graphic.points[0], graphic.comment, canvas);}
+                case "line":
+                {Draw_Line(graphic.color, graphic.points, graphic.comment, canvas);}
+                case "segment":
+                {Draw_Segment(graphic.color, graphic.points, graphic.comment, canvas);}
+                case "ray":
+                {Draw_Ray(graphic.color, graphic.points, graphic.comment, canvas);}
+                case "arc":
+                {Draw_Arc(graphic.color, graphic.points, graphic.rad, graphic.comment, canvas);}
+                case "circle":
+                {Draw_Circle(graphic.color, graphic.points[0], graphic.rad, graphic.comment, canvas);}
+                default:
+                {continue;}
+            }
+        }
     }
 
     private void ClearGraphics(object sender, EventArgs e)
     {
-        //Limpiar el panel de graficos
+        panel.Refresh();
+        //textBox1.Text = "";
     }
 
+    //Metodos para graficar
+
+    private void Draw_Point(Brush brush, Point punto, string comment, Graphics canvas)
+    {
+        canvas.FillEllipse(brush, punto.X , punto.Y , 7, 7);
+        canvas.DrawString(comment, new Font("Arial", 12), Brushes.Black, X, Y);
+    }
+    private void Draw_Arc(Brush brush, Point[] puntos, double radio, string comment, Graphics canvas)
+    {
+        foreach (Point punto in puntos)
+        {
+            Draw_Point(brush, punto, "", canvas);
+        }
+        canvas.DrawArc();
+        canvas.DrawString(comment, new Font("Arial", 12), Brushes.Black, X, Y);
+    }
+
+    private void Draw_Circle(Brush brush, Point punto, double radio, string comment, Graphics canvas)
+    {
+        Draw_Point(brush, punto, "", canvas);       
+        canvas.DrawEllipse(brush, punto.X, punto.Y, radio, radio);
+        canvas.DrawString(comment, new Font("Arial", 12), Brushes.Black, X, Y);
+    }
+    private void Draw_Segment(Brush brush, Point[] puntos, string comment, Graphics canvas)
+    {
+        foreach (Point punto in puntos)
+        {
+            Draw_Point(brush, punto, "", canvas);
+        }
+        canvas.DrawLine(puntos[0],puntos[1]);
+        canvas.DrawString(comment, new Font("Arial", 12), Brushes.Black, X, Y);
+    }
+    private void Draw_Line(Brush brush, Point[] puntos, string comment, Graphics canvas)
+    {
+        foreach (Point punto in puntos)
+        {
+            Draw_Point(brush, punto, "", canvas);
+        }
+        Point[] interceptos = Interceptos(puntos[0],puntos[1]);
+        canvas.DrawLine(interceptos[0], interceptos[1]);
+        canvas.DrawString(comment, new Font("Arial", 12), Brushes.Black, X, Y);
+    }
+    private void Draw_Ray(Brush brush, Point[] puntos, string comment, Graphics canvas)
+    {
+        foreach (Point punto in puntos)
+        {
+            Draw_Point(brush, punto, "", canvas);
+        }
+
+        canvas.DrawString(comment, new Font("Arial", 12), Brushes.Black, X, Y);
+    }
+    
+    public static float Pendiente(Point p1, Point p2)
+    {
+        return (p2.X - p1.X) / (p2.Y - p1.Y);
+    }
+
+    public static Point[] Interceptos(Point p1, Point p2)
+    {
+        float m = Pendiente(p1,p2);
+        float n = p1.Y - m* p1.X ;
+        float cero = -n/m ;
+        return // 0,n y cero,0
+    }
 }
